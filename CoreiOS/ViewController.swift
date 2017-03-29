db7dbf4143f7da1d5bcc5702ab89c21c3b9112cd
szilardinho <szilardinhog@gmail.com>
2017-03-28 22:06:43 -0400

    final test

--- CoreiOS/ViewController.swift
+++ CoreiOS/ViewController.swift
@@ -8,10 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var statePicker: UIPickerView!
+    @IBOutlet weak var statePickerButton: UIButton!
+    
+    let states = ["Hawaii", "New York", "California", "Texas", "Colorado", "Florida"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+       let rectObject = CGRect(x: statePickerButton.frame.maxX + 4, y: statePickerButton.frame.maxY - 8, width: 50, height: 10)
+        
+        let textfield1: UITextField = UITextField(frame: rectObject)
+        textfield1.borderStyle = UITextBorderStyle.roundedRect
+        self.view.addSubview(textfield1)  
+         
+    //Create TextField
+        
+        if statePicker.isHidden == false {
+            textfield1.isHidden = true
+        }
+        
+        statePicker.delegate = self
+        statePicker.dataSource = self
+        
         // Do any additional setup after loading the view, typically from a nib.
         
    //     self.view.backgroundColor = UIColor.cyan
@@ -24,5 +45,30 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func stateButtonPressed(_ sender: Any) {
+        statePicker.isHidden = false
+        
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        statePickerButton.setTitle(states[row], for: UIControlState.normal)
+        statePicker.isHidden = true
+        
+    }
 }
+
+
 
